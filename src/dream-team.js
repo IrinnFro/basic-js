958a8c7939ce3cf9bfea06fb404a09875131d111
IrinnFro <chigchig17@gmail.com>
2020-10-26 23:30:53 +0300

    init: start dreamTeam task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let str = '';
+  let abbr = '';
+  
+  if (!Array.isArray(members)) return false;
+  
+  for (let i = 0; i <= members.length; i++) {
+    if (typeof(members[i]) === 'string') {
+      str = members[i].replace(/ /g, '').toUpperCase();
+      abbr += str[0]
+    }
+  }
+  return abbr.split('').sort().join('');
 };
