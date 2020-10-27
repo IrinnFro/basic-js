ff7292b234b7f85297430bf9accc5255c4fc5e9e
IrinnFro <chigchig17@gmail.com>
2020-10-27 22:36:02 +0300

    init: start count-cats, dream-team, what-season tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(array) {
+ // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let a = array.flat(Infinity);
+  let b = a.filter(i => i === '^^').length;
+  return b;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+ if (!date) return 'Unable to determine the time of year!';
+
+ if ( isNaN( date.getTime())) return 'THROW';
+
+  switch (date.getMonth()) {
+    case 0: return 'winter';
+    break;
+    case 1: return 'winter';
+    break;
+    case 2: return 'spring';
+    break;
+    case 3: return 'spring';
+    break;
+    case 4: return 'spring';
+    break;
+    case 5: return 'summer';
+    break;
+    case 6: return 'summer';
+    break;
+    case 7: return 'summer';
+    break;
+    case 8: return 'autumn';
+    break;
+    case 9: return 'autumn';
+    break;
+    case 10: return 'autumn';
+    break;
+    case 11: return 'winter';
+    break;
+  };
+
 };
